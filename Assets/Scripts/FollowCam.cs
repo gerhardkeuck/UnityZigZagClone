d02b26ba6185e4efecdc6122d4413f0a3881c5ac
Gerhard <gerhard@metalmanagementsolutions.com>
2021-08-18 05:54:17 +0200

    fix gitignore in wrong place

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        // get delta between this camera and the object that is being tracked when object starts.
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
